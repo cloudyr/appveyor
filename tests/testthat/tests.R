383c70a121240574e5d18c37ca5c98afade0b0aa
Thomas J. Leeper <thosjleeper@gmail.com>
2015-10-21 22:55:27 +0100

    outline test suite

---
+++ tests/testthat/tests.R
@@ -0,0 +1,55 @@
+context("Accounts")
+
+test_that("check accounts", {
+  TRUE
+})
+
+
+context("Users")
+
+test_that("check users", {
+  TRUE
+})
+
+test_that("add and delete users", {
+  TRUE
+})
+
+
+test_that("check roles", {
+  TRUE
+})
+
+test_that("add and delete roles", {
+  TRUE
+})
+
+test_that("check collaborators", {
+  TRUE
+})
+
+
+context("repo")
+
+test_that("get repo", {
+  TRUE
+})
+
+test_that("get repo settings", {
+  TRUE
+})
+
+test_that("environment variables", {
+  # get environment variables
+  # set environment variables
+  TRUE
+})
+
+test_that("get build history", {
+  TRUE
+})
+
+test_that("restart and cancel build", {
+  TRUE
+})
+
